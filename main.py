541365c9feb934674f8449baaa1e141613f1d26e
G H <giang.hoang@tu-dortmund.de>
2025-09-16 17:48:14 +0200

    Finished game logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 
 def display_score():
@@ -8,6 +9,30 @@ def display_score():
     score_rect = score_surface.get_rect(center=(670, 50))
     screen.blit(score_surface, score_rect)
     return current_time
+
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:  # check if sth in the list first
+        for obstacle_rect in obstacle_list:
+            # move everything in the rect
+            obstacle_rect.x -= 5  # every obstacle moved to the left by a tiny bit on every cycle of game loop
+
+            if obstacle_rect.bottom == 300:
+                screen.blit(ghost_surface, obstacle_rect)
+            else:
+                screen.blit(fly_surface, obstacle_rect)
+
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        return obstacle_list
+    else:
+        return []
+
+def collision(player, obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): return False
+    return True
+
 
 
 # pygame setup
@@ -29,8 +54,12 @@ score = 0
 # Surfaces
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
+
+# Obstacles
 ghost_surface = pygame.image.load('graphics/ghost.png').convert_alpha()
-ghost_rect = ghost_surface.get_rect(bottomright=(600, 300))
+fly_surface = pygame.image.load('graphics/fly.png').convert_alpha()
+
+obstacle_rect_list = []
 
 # score_surface = test_font.render('Score:', False, 'Black')
 # score_rect = score_surface.get_rect(center = (670,50))
@@ -55,6 +84,10 @@ game_name_rect = game_name.get_rect(center=(400, 60))
 game_message = test_font.render('Press space to run', False, 'Black')
 game_message_rect = game_message.get_rect(center=(400, 330))
 
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1500)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,8 +107,13 @@ while True:
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                ghost_rect.left = 800
+                # ghost_rect.left = 800
                 start_time = int(pygame.time.get_ticks() / 1000)
+        if event.type == obstacle_timer and game_active:
+            if randint(0, 2):  # output 0 or 1 -> false or true
+                obstacle_rect_list.append(ghost_surface.get_rect(bottomright=(randint(900, 1100), 300)))
+            else:
+                obstacle_rect_list.append(ghost_surface.get_rect(bottomright=(randint(900, 1100), 200)))
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
@@ -87,9 +125,9 @@ while True:
         # screen.blit(score_surface,score_rect)
         score = display_score()
 
-        ghost_rect.x -= 4
-        if ghost_rect.right <= 0: ghost_rect.left = 800
-        screen.blit(ghost_surface, ghost_rect)
+        # ghost_rect.x -= 4
+        # if ghost_rect.right <= 0: ghost_rect.left = 800
+        # screen.blit(ghost_surface, ghost_rect)
 
         # Player
         player_gravity += 1
@@ -100,12 +138,18 @@ while True:
         if player_rect.bottom >= 300: player_rect.bottom = 300
         screen.blit(player_surface, player_rect)
 
+        # Obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
         # Collision
-        if ghost_rect.colliderect(player_rect):
-            game_active = False
+        game_active = collision(player_rect, obstacle_rect_list)
+
     else:
         screen.fill('Yellow')
         screen.blit(player_stand, player_stand_rect)
+        obstacle_rect_list.clear()
+        player_rect.midbottom = (80,300)
+        player_gravity = 0
 
         score_message = test_font.render(f'Your score: {score}', False, 'Black')
         score_message_rect = score_message.get_rect(center=(400, 330))
