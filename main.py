eaac555679b47573165513bcf0db3e3e8d31dc7d
G H <giang.hoang@tu-dortmund.de>
2025-09-16 23:03:04 +0200

    Added animations

--- main.py
+++ main.py
@@ -27,12 +27,25 @@ def obstacle_movement(obstacle_list):
     else:
         return []
 
+
 def collision(player, obstacles):
     if obstacles:
         for obstacle_rect in obstacles:
             if player.colliderect(obstacle_rect): return False
     return True
 
+
+def player_animation():
+    # play walking animation if the player is on the floor
+    # or else play jump surface
+    global player_surface, player_index
+
+    if player_rect.bottom < 300:  # if player on the ground or not
+        player_surface = player_jump
+    else:
+        player_index += 0.1  # adding 0.1 to take a little time to change to next index
+        if player_index >= len(players): player_index = 0
+        player_surface = players[int(player_index)]
 
 
 # pygame setup
@@ -56,8 +69,17 @@ sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
 # Obstacles
-ghost_surface = pygame.image.load('graphics/ghost.png').convert_alpha()
-fly_surface = pygame.image.load('graphics/fly.png').convert_alpha()
+ghost_frame_1 = pygame.image.load('graphics/ghost1.png').convert_alpha()
+ghost_frame_2 = pygame.image.load('graphics/ghost2.png').convert_alpha()
+ghost_frames = [ghost_frame_1, ghost_frame_2]
+ghost_frame_index = 0
+ghost_surface = ghost_frames[ghost_frame_index]
+
+fly_frame_1 = pygame.image.load('graphics/fly1.png').convert_alpha()
+fly_frame_2 = pygame.image.load('graphics/fly2.png').convert_alpha()
+fly_frames = [fly_frame_1, fly_frame_2]
+fly_frame_index = 0
+fly_surface = fly_frames[fly_frame_index]
 
 obstacle_rect_list = []
 
@@ -68,13 +90,20 @@ obstacle_rect_list = []
 text_surface = test_font.render('Run for life', False, 'Black')
 ghost_x_position = 30
 
-player_surface = pygame.image.load('graphics/player.png').convert_alpha()
+# Player
+player_1 = pygame.image.load('graphics/player1.png').convert_alpha()
+player_2 = pygame.image.load('graphics/player2.png').convert_alpha()
+players = [player_1, player_2]
+player_index = 0
+player_jump = pygame.image.load('graphics/jump.png').convert_alpha()
+
+player_surface = players[player_index]
 player_rect = player_surface.get_rect(midbottom=(50, 300))
 
 player_gravity = 0
 
 # Intro screen
-player_stand = pygame.image.load('graphics/player.png').convert_alpha()
+player_stand = pygame.image.load('graphics/player1.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
 player_stand_rect = player_stand.get_rect(center=(400, 200))
 
@@ -87,6 +116,12 @@ game_message_rect = game_message.get_rect(center=(400, 330))
 # Timer
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1500)
+
+ghost_animation_timer = pygame.USEREVENT +2
+pygame.time.set_timer(ghost_animation_timer, 500)
+
+fly_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(fly_animation_timer, 200)
 
 while True:
     for event in pygame.event.get():
@@ -109,11 +144,21 @@ while True:
                 game_active = True
                 # ghost_rect.left = 800
                 start_time = int(pygame.time.get_ticks() / 1000)
-        if event.type == obstacle_timer and game_active:
-            if randint(0, 2):  # output 0 or 1 -> false or true
-                obstacle_rect_list.append(ghost_surface.get_rect(bottomright=(randint(900, 1100), 300)))
-            else:
-                obstacle_rect_list.append(ghost_surface.get_rect(bottomright=(randint(900, 1100), 200)))
+        if game_active:
+            if event.type == obstacle_timer:
+                if randint(0, 2):  # output 0 or 1 -> false or true
+                    obstacle_rect_list.append(ghost_surface.get_rect(bottomright=(randint(900, 1100), 300)))
+                else:
+                    obstacle_rect_list.append(ghost_surface.get_rect(bottomright=(randint(900, 1100), 200)))
+            if event.type == ghost_animation_timer:
+                if ghost_frame_index == 0: ghost_frame_index = 1
+                else: ghost_frame_index = 0
+                ghost_surface = ghost_frames[ghost_frame_index]
+
+            if event.type == fly_animation_timer:
+                if fly_frame_index == 0: fly_frame_index = 1
+                else: fly_frame_index = 0
+                fly_surface = fly_frames[fly_frame_index]
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
@@ -136,6 +181,7 @@ while True:
         # 300 is the position of the ground
         # as long as the player exceed in the ground, set the player on the top of the ground
         if player_rect.bottom >= 300: player_rect.bottom = 300
+        player_animation()
         screen.blit(player_surface, player_rect)
 
         # Obstacle movement
@@ -148,7 +194,7 @@ while True:
         screen.fill('Yellow')
         screen.blit(player_stand, player_stand_rect)
         obstacle_rect_list.clear()
-        player_rect.midbottom = (80,300)
+        player_rect.midbottom = (80, 300)
         player_gravity = 0
 
         score_message = test_font.render(f'Your score: {score}', False, 'Black')
